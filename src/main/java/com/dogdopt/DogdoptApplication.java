680125fcf49e65074d2eb5b29724ee7d8b91460e
Mark Matlock <techmatlock@gmail.com>
2025-03-27 10:54:57 -0700

    First commit

---
+++ src/main/java/com/dogdopt/DogdoptApplication.java
@@ -0,0 +1,13 @@
+package com.dogdopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogdoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogdoptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogdopt/DogdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogdopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogdoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
