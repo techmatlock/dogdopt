fe9f33e3c5c1ca45ad2268216340a8d0355fb386
Mark Matlock <techmatlock@gmail.com>
2025-03-27 12:44:43 -0700

    Added get and post endpoints

--- src/main/java/com/dogdopt/controller/DogController.java
+++ src/main/java/com/dogdopt/controller/DogController.java
@@ -3,8 +3,12 @@ package com.dogdopt.controller;
 import com.dogdopt.model.Dog;
 import com.dogdopt.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.net.URI;
 import java.util.List;
 
 public class DogController {
@@ -13,7 +17,13 @@ public class DogController {
     private DogRepository dogRepository;
 
     @GetMapping("/dogs")
-    public List<Dog> getAllItems() {
+    public List<Dog> getAllDogs() {
         return (List<Dog>) dogRepository.findAll();
+    }
+
+    @PostMapping("/dog")
+    public ResponseEntity addDog(@RequestBody Dog dog) {
+        Dog newDog = dogRepository.save(dog);
+        return ResponseEntity.created(URI.create("/" + newDog.getDogId())).build();
     }
 }
