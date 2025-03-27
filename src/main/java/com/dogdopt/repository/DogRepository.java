8440dc9a7cb975e46b8f9af768832780902f9825
Mark Matlock <techmatlock@gmail.com>
2025-03-27 12:45:26 -0700

    Added EnableScan annotation

--- src/main/java/com/dogdopt/repository/DogRepository.java
+++ src/main/java/com/dogdopt/repository/DogRepository.java
@@ -4,7 +4,7 @@ import com.dogdopt.model.Dog;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-@EnableScan
+@EnableScan // allows us to query table with an attribute that's not a partition key
 public interface DogRepository extends
         CrudRepository<Dog, String> {
 }
