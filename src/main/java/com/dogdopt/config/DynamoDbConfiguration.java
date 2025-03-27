b571504bf16a90f45bd7fb8c87cd05e15301066e
Mark Matlock <techmatlock@gmail.com>
2025-03-27 12:08:03 -0700

    Added DynamoDBMapper configuration

---
+++ src/main/java/com/dogdopt/config/DynamoDbConfiguration.java
@@ -0,0 +1,65 @@
+package com.dogdopt.config;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = {"com.dogdopt.repositories"})
+public class DynamoDbConfiguration {
+
+    @Value("${aws.dynamodb.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.dynamodb.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.dynamodb.region}")
+    private String region;
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String endpoint;
+
+    private AWSCredentialsProvider awsDynamoDBCredentials() {
+        return new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(accessKey, secretKey));
+    }
+
+    @Primary
+    @Bean
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    /*
+    Constructs new mapper with service object and config given
+    Enables us to perform CRUD operations and run queries and scans
+    ... against tables
+    */
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB,
+                                         DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withCredentials(awsDynamoDBCredentials()).build();
+    }
+}
+
