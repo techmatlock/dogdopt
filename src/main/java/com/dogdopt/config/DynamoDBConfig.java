9396dfd44c7cf466b2211269e86b5d0c0742af3a
Mark Matlock <techmatlock@gmail.com>
2025-03-27 11:32:14 -0700

    Added imports

--- src/main/java/com/dogdopt/config/DynamoDBConfig.java
+++ src/main/java/com/dogdopt/config/DynamoDBConfig.java
@@ -1,5 +1,15 @@
 package com.dogdopt.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
 @Configuration
 @EnableDynamoDBRepositories
   (basePackages = "com.baeldung.spring.data.dynamodb.repositories")
