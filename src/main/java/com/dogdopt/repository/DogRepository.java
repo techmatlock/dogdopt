8098b5bfb388af4a987e25eb81a4158437dc2924
Mark Matlock <techmatlock@gmail.com>
2025-03-28 10:58:54 -0700

    Improved comments

--- src/main/java/com/dogdopt/repository/DogRepository.java
+++ src/main/java/com/dogdopt/repository/DogRepository.java
@@ -5,10 +5,10 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Repository to connect with DynamoDB and fetch data with queries from
- * dogs table
+ * Interface to connect with DynamoDB to read and persist data
  */
-@EnableScan
+
+@EnableScan // allows us to query table with an attribute that's not a partition key
 public interface DogRepository extends
         CrudRepository<Dog, String> {
 }
