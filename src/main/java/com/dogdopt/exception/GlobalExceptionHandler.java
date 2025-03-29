ea517b473f59d3c73359f2f20c5cf9a4311523ad
Mark Matlock <techmatlock@gmail.com>
2025-03-28 17:59:07 -0700

    Added handleAllExceptions

---
+++ src/main/java/com/dogdopt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.dogdopt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception e) {
+        return new ResponseEntity<>("An unexpected error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
